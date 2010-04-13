06dcbfdbe733ef1ab91d86c1b918dc333d7c26e7
Nando Vieira <fnando.vieira@gmail.com>
2010-04-13 19:00:57 +0800

    Added test-unit gem

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,10 @@
 $:.unshift(File.dirname(__FILE__) + '/../lib')
-require 'test/unit'
 
 begin
   RAILS_ENV = "test"
   require File.expand_path(File.join(File.dirname(__FILE__), '../../../../config/environment.rb'))
+  gem "test-unit"
+  require 'test/unit'
   require 'action_controller/test_process'
 rescue LoadError
   puts "\n[!] Tests only run when the plugin is installed in the plugins directory, running the tests against anything other than your particular Rails version doesn't really serve a purpose."
